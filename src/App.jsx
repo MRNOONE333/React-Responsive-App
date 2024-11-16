fdbde6fe2b494263400ce2e0788ea99a2e2e538b
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-11-16 18:51:08 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Herosection from "./components/Herosection";
 
 const app = () =>{
   return(
     <>
       <Navbar/>
+      <div className="max-w-7xl mx-auto pt-20 px-6 ">
+        <Herosection/>
+      </div>
+      
     </>
   )
 }
---
+++ src/components/Herosection.jsx
@@ -0,0 +1,16 @@
+import video1 from "../assets/video1.mp4" ;
+import video2 from "../assets/video2.mp4" ;
+
+const Herosection = () => {
+  return (
+    <div className="flex flex-row items-center mt-6 lg:mt-20">
+        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+            Best NFT built Tool
+            <span className="bg-gradient-to-r from-orange-500 to-red-800 bg-clip-text text-transparent">
+                {" "}For developers</span>
+        </h1>
+    </div>
+  )
+}
+
+export default Herosection
