f6f804d772d3b885fff2f58a2e364317035d93ab
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-11-18 20:30:35 +0530

    feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Herosection from "./components/Herosection";
+import FeatureSection from "./components/FeatureSection";
 
 const app = () =>{
   return(
@@ -8,6 +9,7 @@ const app = () =>{
       <Navbar/>
       <div className="max-w-7xl mx-auto pt-20 px-6 ">
         <Herosection/>
+        <FeatureSection/>
       </div>
       
     </>
