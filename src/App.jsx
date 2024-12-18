8b53600b1d4da162dc216afc18f660f27b10ba8c
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-12-18 23:22:16 +0530

    added prcing menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Herosection from "./components/Herosection";
 import FeatureSection from "./components/FeatureSection";
-
+import Pricing from "./components/Pricing";
 const app = () =>{
   return(
     <>
@@ -10,6 +10,7 @@ const app = () =>{
       <div className="max-w-7xl mx-auto pt-20 px-6 ">
         <Herosection/>
         <FeatureSection/>
+        <Pricing />
       </div>
       
     </>
